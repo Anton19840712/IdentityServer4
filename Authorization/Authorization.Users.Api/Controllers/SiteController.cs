de20587bde6264067007a8309264edec881066d4
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-12-12 19:00:25 +0300

    Get identity model.

--- Authorization/Authorization.Users.Api/Controllers/SiteController.cs
+++ Authorization/Authorization.Users.Api/Controllers/SiteController.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Threading.Tasks;
+using IdentityModel.Client;
 
 namespace Authorization.Users.Api.Controllers
 {
     [Route("[controller]")]
     public class SiteController : Controller
     {
-        private IHttpClientFactory _httpClientFactory;
+        private readonly IHttpClientFactory _httpClientFactory;
 
         public SiteController(IHttpClientFactory httpClientFactory)
         {
@@ -24,6 +25,14 @@ namespace Authorization.Users.Api.Controllers
         [Route("[action]")]
         public async Task<IActionResult> GetOrders()
         {
+            //retrieve to Identity4Server
+            var authClient = _httpClientFactory.CreateClient();
+
+            //за счет пакета IdentityModel = выбираешь то, что ты получил на странице по запросу localhost: 10001/.well-known/openid-configuration
+
+            var discoveryDocument = await authClient.GetDiscoveryDocumentAsync("https://localhost:10001");
+
+            //retrieve to orders api
             var ordersClient = _httpClientFactory.CreateClient();
 
             var response = await ordersClient.GetAsync("https://localhost:5001/site/getSecret");
