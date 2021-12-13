af8b80fc752b5b897b09fe50c41aac89b26fa672
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-12-13 20:10:48 +0300

    Add some constraints for api.

--- Authorization/Authorization.Client.Mvc/Controllers/SiteController.cs
+++ Authorization/Authorization.Client.Mvc/Controllers/SiteController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Authorization.Client.Mvc.Controllers
 {
+    [Route("[controller]")]
     public class SiteController : Controller
     {
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [Route("[action]")]
         public IActionResult Index()
         {
             return View();
