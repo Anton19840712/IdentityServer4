38a5cf96389497ef53a28a99018049683dfa8a36
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-12-14 10:28:14 +0300

    Add url for service identity.

--- Authorization/Authorization.IdentityServer/Startup.cs
+++ Authorization/Authorization.IdentityServer/Startup.cs
@@ -11,11 +11,15 @@ namespace Authorization.IdentityServer
         {
             //add package identityServer4, then add app.UseIdentity, then services.AddIdentityServer();
             //could work in in-memory or database style
-            services.AddIdentityServer()
+            services.AddIdentityServer(options =>
+                {
+                    options.UserInteraction.LoginUrl = "/Auth/Login"; //this url will be used as path to go on this identity server.
+                })
                 .AddInMemoryClients(Configuration.GetClients())//Xamarin, Console, Angular, etc.
                 .AddInMemoryApiResources(Configuration.GetApiResources()) //
                 .AddInMemoryIdentityResources(Configuration.GetIdentityResources()) //
                 .AddDeveloperSigningCredential();//not certificates, but something like plug
+
             services.AddControllersWithViews();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
