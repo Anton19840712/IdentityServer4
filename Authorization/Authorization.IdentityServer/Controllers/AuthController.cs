2fd20541ad44e8e6d057b362a032959b666dea41
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-12-14 10:26:17 +0300

    Change auth controller.

---
+++ Authorization/Authorization.IdentityServer/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Authorization.IdentityServer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.IdentityServer.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        public AuthController()
+        {
+            
+        }
+
+        [Route("action")]
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View();
+        }
+    }
+}
