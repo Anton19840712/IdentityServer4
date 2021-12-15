8055285c821ad7d5206997e81f87154dc5fad05b
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-12-15 12:06:10 +0300

    Move add db context up to avoid bug of adding user.

--- Authorization/Authorization.IdentityServer/Startup.cs
+++ Authorization/Authorization.IdentityServer/Startup.cs
@@ -1,5 +1,8 @@
+using Authorization.IdentityServer.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,16 +12,32 @@ namespace Authorization.IdentityServer
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(config =>
+            {
+                config.UseInMemoryDatabase("MEMORY");
+            })
+            .AddIdentity<IdentityUser, IdentityRole>(config => //this should be before .AddAspNetIdentity<IdentityUser>(), because it could be mistake something like add 1 user
+                                                               //IUserClaimsPrincipalFactory`1 not registered error in IndentityServer implementation
+            {
+                config.Password.RequireDigit = false;
+                config.Password.RequireLowercase = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+                config.Password.RequiredLength = 6;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
             //add package identityServer4, then add app.UseIdentity, then services.AddIdentityServer();
             //could work in in-memory or database style
             services.AddIdentityServer(options =>
-                {
-                    options.UserInteraction.LoginUrl = "/Auth/Login"; //this url will be used as path to go on this identity server.
-                })
-                .AddInMemoryClients(Configuration.GetClients())//Xamarin, Console, Angular, etc.
-                .AddInMemoryApiResources(Configuration.GetApiResources()) //
-                .AddInMemoryIdentityResources(Configuration.GetIdentityResources()) //
-                .AddDeveloperSigningCredential();//not certificates, but something like plug
+            {
+                options.UserInteraction.LoginUrl = "/Auth/Login"; //this url will be used as path to go on this identity server.
+            })
+            .AddAspNetIdentity<IdentityUser>()
+            .AddInMemoryClients(Configuration.GetClients())//Xamarin, Console, Angular, etc.
+            .AddInMemoryApiResources(Configuration.GetApiResources()) //
+            .AddInMemoryIdentityResources(Configuration.GetIdentityResources()) //
+            .AddDeveloperSigningCredential();//not certificates, but something like plug
 
             services.AddControllersWithViews();
         }
