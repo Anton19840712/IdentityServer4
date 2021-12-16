3cec914822e269e678825d65a2c70c69e8e72812
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-12-16 10:12:50 +0300

    Add claim manager.

---
+++ Authorization/Authorization.Client.Mvc/ViewModels/IClaimManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Client.Mvc.ViewModels
+{
+    public interface IClaimManager<T>
+    {
+        void AddTokenInfo(string nameToken, T element);
+    }
+}
