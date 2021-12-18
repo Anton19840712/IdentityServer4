331614e6f8ebbedf9dc2a1a2795cc4766f265258
Anton <anton.gridushko@gmail>
2021-12-18 17:00:28 +0300

    Implement method to get orders.

--- Is_4/Authorization/Authorization.Users.Api/Controllers/UserController.cs
+++ Is_4/Authorization/Authorization.Users.Api/Controllers/UserController.cs
@@ -2,19 +2,33 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Authorization.Users.Api.Controllers
 {
+    //[Route("controller")]
     public class UserController : Controller
     {
+        private readonly IHttpClientFactory _client;
+
+        public UserController(IHttpClientFactory client)
+        {
+            _client = client;
+        }
+        //[Route("action")]
         public IActionResult Index()
         {
             return View();
         }
-        public string GetOrders()
+        //[Route("action")]
+        public async Task<string> GetOrders()
         {
-            return "These are my orders.";
+            var ordersClient = _client.CreateClient();
+
+            var response = await ordersClient.GetAsync("https://localhost:5001/order/getOrders");
+
+            return $"These are my orders: {await response.Content.ReadAsStringAsync()}";
         }
     }
 }
