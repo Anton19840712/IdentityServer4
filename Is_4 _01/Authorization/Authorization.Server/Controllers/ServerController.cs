75a0d329265d753b7b215674b3e4db670a133a56
Anton <anton.gridushko@gmail>
2021-12-18 17:05:47 +0300

    Remove extra usings.

--- Is_4 _01/Authorization/Authorization.Server/Controllers/ServerController.cs
+++ Is_4 _01/Authorization/Authorization.Server/Controllers/ServerController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Authorization.Server.Controllers
 {
-    public class SiteController : Controller
+    public class ServerController : Controller
     {
         public IActionResult Index()
         {
